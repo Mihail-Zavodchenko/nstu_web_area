9a26271e0b82ba86a2406570289656807cd183a0
zavodchenko.2014 <zavodchenko.2014@PMI-208B-10>
2017-03-10 09:45:19 +0700

    что-то добавлено

---
+++ area_sharp/area_sharp/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_sharp
+{
+    class Circle
+    {
+        int radius;
+        public void squareCircle()
+        {
+            Console.Write((Math.PI * radius * radius).ToString());
+        }
+
+    }
+}
